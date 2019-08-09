14525405c92f25c49ba1c5bb68d0aeb29fb30b56
= <=>
2019-08-09 21:13:00 +0700

    Add casting.py file

---
+++ casting.py
@@ -0,0 +1,23 @@
+x = int(1)
+y = int(2.5)
+z = int('25')
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = float(1)
+y = float(2.5)
+z = float('25')
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = str(1)
+y = str(2.5)
+z = str('25')
+
+print(type(x))
+print(type(y))
+print(type(z))
