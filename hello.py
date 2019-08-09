bcbce7da42d2da988132e976d0ef8087757127fa
= <=>
2019-08-09 20:35:16 +0700

    Add variables.py file

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print('Hello, world!')
+if 5 > 2 :
+	print('Five is greater than two!')
+
+x = 5
+y = 'Hello, world!'
+
+print(x)
+print(y)
+
+# This is a single line comment
+"""
+This is also comment with multiple of line
+"""
+print('Under comment')
---
+++ variables.py
@@ -0,0 +1,31 @@
+age  = 25
+name = 'M. Akbar Nugroho'
+
+print(age)
+print(name)
+
+changeble_var = 3
+changeble_var = 'Nadziroh'
+
+print(changeble_var)
+
+x, y, z = 'Orange', 'Banana', 'Cherry'
+
+print(x)
+print(y)
+print(z)
+
+a = b = c = 'Fruits'
+
+print(a)
+print(b)
+print(c)
+
+d = 'Cool!'
+
+print('Python is ' + d)
+
+five = 5
+two  = 2
+
+print(five + two)
