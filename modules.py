971875fd6cd2c195c8d6d2462ddfd3c97a418047
= <=>
2019-08-10 19:02:15 +0700

    Add modules, modules.py file

---
+++ modules.py
@@ -0,0 +1,11 @@
+from modules.greeting import greet
+from modules.greeting import person
+import platform
+
+greet('M. Akbar Nugroho')
+
+print(platform.system())
+print(platform.processor())
+print(platform.python_version())
+
+print(person)
---
+++ modules/greeting.py
@@ -0,0 +1,8 @@
+person = {
+	'name'    : 'M. Akbar Nugroho',
+	'age'     : 25,
+	'country' : 'Indonesia'
+}
+
+def greet(name = 'John Doe') :
+	print(name)
