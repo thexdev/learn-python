fcaba2b746541e75537a558f372164d6dd50ded7
= <=>
2019-08-10 11:10:21 +0700

    Add operators.py file

---
+++ operators.py
@@ -0,0 +1,71 @@
+# Python arithmetic operators
+print(1 + 1)  # Addition
+print(2 - 1)  # Substraction
+print(2 * 5)  # Multiplication
+print(6 / 2)  # Division
+print(10 % 4) # Modulus
+print(5 ** 2) # Exponentiation
+print(6 // 2) # Floor Division
+
+# Python assignment operators
+x = 5
+print(x)
+
+x += 3
+print(x)
+
+x -= 3
+print(x)
+
+x *= 3
+print(x)
+
+x /= 3
+print(x)
+
+x %= 3
+print(x)
+
+x //= 3
+print(x)
+
+x **= 3
+print(x)
+
+x &= 3
+print(x)
+
+x |= 3
+print(x)
+
+x ^= 3
+print(x)
+
+x >>= 3
+print(x)
+
+x <<= 3
+print(x)
+
+# Python comparation operators
+print(x == 5)
+print(x != 5)
+print(x > 5)
+print(x < 5)
+print(x >= 5)
+print(x <= 5)
+
+
+# Python logical operators
+print(x < 5 and x < 10)
+print(x < 4 or x == 0)
+print(not(x < 4))
+
+# Python identity operators
+print(x is 0)
+print(x is not 0)
+
+# Python membership operators
+print('a' in 'akbar')
+print('x' not in 'akbar')
+
