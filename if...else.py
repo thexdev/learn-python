775657f1608b9fb45ba645c34d5a52457d168b32
= <=>
2019-08-10 15:47:32 +0700

    Add if...else.py file

---
+++ if...else.py
@@ -0,0 +1,19 @@
+x = 25
+y = 3
+
+if x < y :
+	print('x is less than y')
+elif x == y :
+	print('x and y are equal')
+else :
+	print('x is greater than y')
+
+a = 1
+b = 4
+
+if b > a :
+	print('b is greater than a')
+else :
+	print('b is not greater than a')
+
+if a < b : print('a is less than b')
